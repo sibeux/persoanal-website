88c8d7ab562eeed908a98156920f958e0fc31ed4
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-02-24 23:09:59 +0700

    Fix formatting in models.php

--- module-akademi-crypto/data/models.php
+++ module-akademi-crypto/data/models.php
@@ -12,7 +12,7 @@ $cryptocurrency_investing = array(
         "Bitcoin a Ponzi Scheme",
         "https://scontent.fmlg5-1.fna.fbcdn.net/v/t1.15752-9/429132496_6657014137734116_7240000763802707243_n.png?_nc_cat=106&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeF0Xb-kUL_r9KerzwQtDw2PLa_QsaUsDZctr9CxpSwNl9_BiPQSZrBn7wzrFmO3ajAImvxra0L3R_Omg_r4eEit&_nc_ohc=hNGG1hrFU68AX9jRs7y&_nc_ht=scontent.fmlg5-1.fna&oh=03_AdSZV7n8MaXXM2NYinGaqFID9LAcfNxUv1OqWJD-zgXiHA&oe=66013B40"
     ),
-    
+
     array(
         "https://www.youtube.com/watch?v=RD6yXZjy08E",
         "Crypto Basics",
@@ -23,6 +23,12 @@ $cryptocurrency_investing = array(
         "https://www.youtube.com/watch?v=eJH60oIboCk",
         "Mindset Crypto Gambler's Mentality",
         "https://scontent.fmlg5-1.fna.fbcdn.net/v/t1.15752-9/422198983_1133728594467769_286555278813383842_n.png?_nc_cat=111&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeGDDdQJ0tXkaHEKzl3sOQGkicJRwK7Cj4uJwlHArsKPi-BOyo7V49iGJ9EikOcSMOGD25hpQyMJWNVsulixuHdd&_nc_ohc=7ddQk8kGJiUAX-KUhi5&_nc_ht=scontent.fmlg5-1.fna&oh=03_AdTx4XsJOfep_FbsUFp1qACLoWMaW9-TM9V9hU8f7tbnuw&oe=66018C2C"
+    ),
+
+    array(
+        "https://www.youtube.com/watch?v=4hZMKsKFtps",
+        "Market Cycles",
+        "https://scontent.fmlg5-1.fna.fbcdn.net/v/t1.15752-9/423422556_405241542190983_1218342507479156224_n.png?_nc_cat=107&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeHOiGMv1flgcRCrlPY2GTV5NZRqLDelhK41lGosN6WEro6aHlID-Oir-tClWLjYJgG9JCOaf9kLIcw4M7Gjyg-_&_nc_ohc=PZtX1zG9Jj8AX96Feuv&_nc_ht=scontent.fmlg5-1.fna&oh=03_AdQalvg3OUFiHvgq3RnGk4DU0ECo9bct0sYDKtRZUUO68A&oe=66016DF4"
     )
 
 );

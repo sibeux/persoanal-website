cb304471f34b97a9d902d00464a5b588cf121ef1
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-05-15 17:49:30 +0700

    Refactor video thumbnail logic in index.php

--- module-akademi-crypto/data/models.php
+++ module-akademi-crypto/data/models.php
@@ -3,107 +3,107 @@
 // 1. Cryptocurrency Investing
 $cryptocurrency_investing = array(
     array(
-        "https://www.youtube.com/watch?v=f0Mvq_xb4OE",
+        "https://youtu.be/f0Mvq_xb4OE",
         "Big Crypto Thesis",
     ),
 
     array(
-        "https://www.youtube.com/watch?v=kJw9EwYIAEA",
+        "https://youtu.be/kJw9EwYIAEA",
         "Bitcoin a Ponzi Scheme",
     ),
 
     array(
-        "https://www.youtube.com/watch?v=RD6yXZjy08E",
+        "https://youtu.be/RD6yXZjy08E",
         "Crypto Basics",
         "https://scontent.fmlg5-1.fna.fbcdn.net/v/t1.15752-9/429118640_415685864243226_4474005988128704942_n.png?_nc_cat=103&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeHkAInGo8qS6K3HHoiTyX7Fl1incD4Q8ZKXWKdwPhDxkr41YJlr0rSt6u8gZ6fRmCl-0YS-PckpBou9iZUC12O0&_nc_ohc=af30RvLd-ggAX-0TXpJ&_nc_ht=scontent.fmlg5-1.fna&oh=03_AdRd-74al1A10wFdZ0sa_JTgsF1Eu1JjEFopfmy02icOVw&oe=66016555"
     ),
 
     array(
-        "https://www.youtube.com/watch?v=eJH60oIboCk",
+        "https://youtu.be/eJH60oIboCk",
         "Mindset Crypto Gambler's Mentality",
         "https://scontent.fmlg5-1.fna.fbcdn.net/v/t1.15752-9/422198983_1133728594467769_286555278813383842_n.png?_nc_cat=111&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeGDDdQJ0tXkaHEKzl3sOQGkicJRwK7Cj4uJwlHArsKPi-BOyo7V49iGJ9EikOcSMOGD25hpQyMJWNVsulixuHdd&_nc_ohc=7ddQk8kGJiUAX-KUhi5&_nc_ht=scontent.fmlg5-1.fna&oh=03_AdTx4XsJOfep_FbsUFp1qACLoWMaW9-TM9V9hU8f7tbnuw&oe=66018C2C"
     ),
 
     array(
-        "https://www.youtube.com/watch?v=4hZMKsKFtps",
+        "https://youtu.be/4hZMKsKFtps",
         "Market Cycles",
         "https://scontent.fmlg5-1.fna.fbcdn.net/v/t1.15752-9/423422556_405241542190983_1218342507479156224_n.png?_nc_cat=107&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeHOiGMv1flgcRCrlPY2GTV5NZRqLDelhK41lGosN6WEro6aHlID-Oir-tClWLjYJgG9JCOaf9kLIcw4M7Gjyg-_&_nc_ohc=PZtX1zG9Jj8AX96Feuv&_nc_ht=scontent.fmlg5-1.fna&oh=03_AdQalvg3OUFiHvgq3RnGk4DU0ECo9bct0sYDKtRZUUO68A&oe=66016DF4"
     ),
 
     array(
-        "https://www.youtube.com/watch?v=6wbCV1Mtz0E",
+        "https://youtu.be/6wbCV1Mtz0E",
         "Types of Cryptocurrencies",
         "https://scontent.fmlg5-1.fna.fbcdn.net/v/t1.15752-9/423221214_382700227805293_2594867661634066446_n.png?_nc_cat=106&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeF4GjfeE4KzZFz2C7Iop3N8RQqAbJIxd15FCoBskjF3XlIJxQExve70pj8zM8_yjKHumZ2CZARV2r64Clha6p5H&_nc_ohc=4S4JeicMz90AX9kD70Y&_nc_ht=scontent.fmlg5-1.fna&oh=03_AdStwAvhr_osWYazoaZmzyGiyQld_v93gI2k86wbNoM0vA&oe=66016DEE"
     ),
 
     array(
-        "https://www.youtube.com/watch?v=_Y2c1fE0Tx8",
+        "https://youtu.be/_Y2c1fE0Tx8",
         "Attention Economy in Crypto",
         "https://scontent.fmlg5-1.fna.fbcdn.net/v/t1.15752-9/429332426_417736767433885_987788558037527112_n.png?_nc_cat=105&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeHC8hpT4mMZBUKjLuBUDUVvYgtRFwh24ZtiC1EXCHbhm5-VF9msUkIkTsvIhDi4ycwGJgfXiuWAJhAmbxHk3XLM&_nc_ohc=B57MloV-WKAAX8dnwhs&_nc_ht=scontent.fmlg5-1.fna&oh=03_AdTLqA0H_kNTVNNa_ht5qbbD4il2Rnhx88JMsGzbkfp_iw&oe=6603CF15"
     ),
 
     array(
-        "https://www.youtube.com/watch?v=iuVT58rYeW0",
+        "https://youtu.be/iuVT58rYeW0",
         "How to Research Crypto",
         "https://scontent.fmlg5-1.fna.fbcdn.net/v/t1.15752-9/429620822_778389794215119_6679682828548772169_n.png?_nc_cat=101&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeHoab39u_JJcPxo-BoKMu6fnMtMIjkB5tScy0wiOQHm1GOyZmD576CoSQf28hrS1iVEuuosIZSMNRlCK0h9yTGT&_nc_ohc=3AQWCjC8M98AX-Gr3qa&_nc_ht=scontent.fmlg5-1.fna&oh=03_AdT6jdanmKgpEFFTlI9tITc1RBNqR8i1ZfnQfQtquqobiQ&oe=6603DF94"
     ),
 
     array(
-        "https://www.youtube.com/watch?v=Mg3CIg6fkYI",
+        "https://youtu.be/Mg3CIg6fkYI",
         "How to Buy Crypto",
         "https://scontent.fmlg5-1.fna.fbcdn.net/v/t1.15752-9/429644135_410510401500415_7319446311866187829_n.png?_nc_cat=108&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeHIHOYisQA_i8zkLjmUGfSbqnH-BOqL1SCqcf4E6ovVIOts1Fq4wFN2ZM5gAKDzKQmYPF-ZW2RcShW01eWhBeEM&_nc_ohc=yrkhrTj90LUAX-WtxjV&_nc_ht=scontent.fmlg5-1.fna&oh=03_AdT3IOAP4daTpfN5rh0P019cP1EVSq3AHohcuOqReTGS9g&oe=6603D4ED"
     ),
 
     array(
-        "https://www.youtube.com/watch?v=iSUTD20VeJI",
+        "https://youtu.be/iSUTD20VeJI",
         "Risk & Portfolio Management",
         "https://scontent.fmlg5-1.fna.fbcdn.net/v/t1.15752-9/429308252_283605727910824_83550433317628757_n.png?_nc_cat=109&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeFaw1sQgsYWcCDv1H7CjqJlX18Tv8D7M_VfXxO_wPsz9YSQybyFBa-uFDq50XKSG3BLm1PavzssIe2xlFifu1dP&_nc_ohc=47cqNN3rJ-0AX8-JP-m&_nc_ht=scontent.fmlg5-1.fna&oh=03_AdQFewlhqqlq63sgz6clsWTDkyi8J-FDGICa_PtuOhXQjg&oe=6603BC83",
     ),
 
     array(
-        "https://www.youtube.com/watch?v=j_K7EIAQKqw",
+        "https://youtu.be/j_K7EIAQKqw",
         "NFT Fundamentals",
         "https://scontent.fmlg5-1.fna.fbcdn.net/v/t1.15752-9/429370411_2179536225728373_6106565422506509324_n.png?_nc_cat=105&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeGzsIVsjkWtX-G6R3THtqKACbcaXGaLzAgJtxpcZovMCPFyN4YBtCiK310Rx7xZdiQxLLGNXf77AoCYmL1xS-ts&_nc_ohc=T9HdpujLbxYAX8MQF8g&_nc_ht=scontent.fmlg5-1.fna&oh=03_AdQRUeam2XbVPjA8nbgIupTKEU1LkKmZoGof7sff_kDXzA&oe=6603C6FF"
     ),
 
     array(
-        "https://www.youtube.com/watch?v=wPJlFJxZ13E",
+        "https://youtu.be/wPJlFJxZ13E",
         "Spot And Futures Market",
         "https://scontent.fmlg5-1.fna.fbcdn.net/v/t1.15752-9/429757945_2044932232545712_243959622747317787_n.png?_nc_cat=103&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeFF8B_NHyQ_d1mD-BxVFDKs0Y0pH1MglZjRjSkfUyCVmJ905t6VBn9PQfwMq6oANyJ2hqey7REfn18bbAxVCOjA&_nc_ohc=um7wsFOTgpsAX_PVZL9&_nc_ht=scontent.fmlg5-1.fna&oh=03_AdTw4wsiY9FluJEZ_NDtB7vZnw1Pf6nv8CtK_KIDCME3vA&oe=6604E2EF"
     ),
 
     array(
-        "https://www.youtube.com/watch?v=5IqHpOuZHL4",
+        "https://youtu.be/5IqHpOuZHL4",
         "Trade Probabilistic Thinking",
         "https://scontent.fmlg5-1.fna.fbcdn.net/v/t1.15752-9/429041995_263097443406510_3035113033285776916_n.png?_nc_cat=105&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeGC5Em7WWiSb9WFVaAIQn2o9MCn1uRZVQX0wKfW5FlVBTJtRxJawsdXkrpI0beyoEG-Cm9jiMRrP6L-CS1qCY5r&_nc_ohc=6H05NB5leQUAX8jNMBZ&_nc_ht=scontent.fmlg5-1.fna&oh=03_AdS8pHKFk3aVoHU8I3yJPncO19UjR5VV85jPfPpCTMAtRQ&oe=6604EE6F"
     ),
 
     array(
-        "https://www.youtube.com/watch?v=oqct0lQf4Ps",
+        "https://youtu.be/oqct0lQf4Ps",
         "Bitcoin & Altcoin Correlation",
         "https://scontent.fmlg5-1.fna.fbcdn.net/v/t1.15752-9/429625958_1452414845651324_2160289948102163037_n.png?_nc_cat=107&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeGNByI1fgPglk3zQDascw3uq1n5D1wG_xyrWfkPXAb_HMCFY2MxAE-E9ufDhk8b9pBiSo7ebio70slpmYI8KDK_&_nc_ohc=b77LBkaLwqkAX_CWQkV&_nc_ht=scontent.fmlg5-1.fna&oh=03_AdST3Y5LnHypXTOORw4SWxVEwEjSeMagjaDLi2Za_uUXAQ&oe=6604DBED"
     ),
 
     array(
-        "https://www.youtube.com/watch?v=-Fs0bxLbemY",
+        "https://youtu.be/-Fs0bxLbemY",
         "Crypto Tools",
         "https://scontent.fmlg5-1.fna.fbcdn.net/v/t1.15752-9/429124938_414782357700368_5218385822403132823_n.png?_nc_cat=106&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeFJEPPmRTIkvjVatj0h3nw_5Vpnov0aWbvlWmei_RpZuwiC57QyWiCbwiF842ogmMNQeFIGI2a30XwXgQawZ5WM&_nc_ohc=puocjuFXglAAX9fH_vn&_nc_ht=scontent.fmlg5-1.fna&oh=03_AdQe6haE35da460mE0R8sdqXeDnV_0i54wVpEmcphg9ylg&oe=6604F45B"
     ),
 
     array(
-        "https://www.youtube.com/watch?v=1svaUWmmRaQ",
+        "https://youtu.be/1svaUWmmRaQ",
         "Smart Money in Crypto",
         "https://scontent.fmlg5-1.fna.fbcdn.net/v/t1.15752-9/429813644_7088611151260582_3976966621062481963_n.png?_nc_cat=105&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeFbrc5DTBcC9AUHOemmo_N4RNF83nqNokVE0Xzeeo2iRW4mdEGxAGkJ9qzeXv-zyRhxOvTiLv0fkJP8AOqZvBmm&_nc_ohc=0Xxf51oF5DcAX-EYSi_&_nc_ht=scontent.fmlg5-1.fna&oh=03_AdRYFm2JLhTAlBn2csgkzZ02Zd5XOWPfNJZSQb_9vtiOsQ&oe=6604E7DB"
     ),
 
     array(
-        "https://www.youtube.com/watch?v=GfSywV-CYXA",
+        "https://youtu.be/GfSywV-CYXA",
         "Portfolio Strategy 2023 Cheat Sheet",
         "https://scontent.fmlg5-1.fna.fbcdn.net/v/t1.15752-9/429092181_966610158405774_3813469682839133614_n.png?_nc_cat=106&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeFOm22SLViyaG92WROm568FobB1jkRqw6mhsHWORGrDqbQybPOB2YWOIK3ZVSy7CZpvNZfD1KLTVBfDA-NQiY1w&_nc_ohc=khJIrKKhwLsAX-kFiOt&_nc_ht=scontent.fmlg5-1.fna&oh=03_AdSAJ1PXI1LAiAcWNe-LajA5TFjoMATPi37JV6DMI3Ykkg&oe=6604E95D"
     ),
 
     array(
-        "https://www.youtube.com/watch?v=gU62X9JSxRQ",
+        "https://youtu.be/gU62X9JSxRQ",
         "Crypto Staking Strategy",
         "https://scontent.fmlg5-1.fna.fbcdn.net/v/t1.15752-9/429611584_1143715463665433_6023516955569260324_n.png?_nc_cat=101&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeGL7lKH_wk1cUHquWYtYv69Z1KP2Gwc2wtnUo_YbBzbC6EOWVZmgfjHCwcgUy1bi5OqiBqkjxFZCtqboTR5ZJtR&_nc_ohc=EBX_32J6a9IAX9LEq9b&_nc_ht=scontent.fmlg5-1.fna&oh=03_AdTh3Ndy1XI5RCCIKaXMlVUWXe_i6aFNScB7ZfZtIsrLXg&oe=6604FC91"
     )

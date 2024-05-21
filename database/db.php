94054ee461c41e9d472364a0facf791e69140def
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-05-22 01:17:46 +0700

    Refactor database connection credentials

--- database/db.php
+++ database/db.php
@@ -1,9 +1,9 @@
 <?php
 
 define('HOST', 'localhost');
-define('SIBEUX', 'sibeuxm1');
-define('pass', 'pMklM1W36;oI.4');
-define('DB', 'sibeuxm1_web_porto');
+define('SIBEUX', '-');
+define('pass', '-');
+define('DB', '-');
 $db = new mysqli(HOST, SIBEUX, pass, DB);
 
 if($db->connect_errno){

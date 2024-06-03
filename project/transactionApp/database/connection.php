ccbe368f57250560058902268bfae81cd5f74b2e
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-06-04 05:04:42 +0700

    p

--- project/transactionApp/database/connection.php
+++ project/transactionApp/database/connection.php
@@ -3,12 +3,12 @@
 define('HOST', 'localhost');
 define('SIBEUX', 'sibk1922_cbux');
 define('pass', '1NvgEHFnwvDN96');
-define('DB', 'sibk1922_cloud_music');
+define('DB', 'sibk1922_pbweb_cashflow');
 
 $db = new mysqli(HOST, USER, pass, DB);
 
 if ($db->connect_errno) {
-    die('Tidak dapat terhubung ke database');
+    die('Tidak dapat terhubung ke database! Silahkan coba lagi nanti.');
 }
 
 $db->set_charset('utf8mb4');

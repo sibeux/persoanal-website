d0e04d6d3feed7dffbb38fc5444b8a2d001dd236
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-06-04 05:00:29 +0700

    Refactor database connection credentials

--- project/transactionApp/database/connection.php
+++ project/transactionApp/database/connection.php
@@ -3,7 +3,7 @@
 define('HOST', 'localhost');
 define('SIBEUX', 'sibk1922_cbux');
 define('pass', '1NvgEHFnwvDN96');
-define('DB', 'pbweb_cashflow');
+define('DB', 'sibk1922_pbweb_cashflow');
 
 $db = new mysqli(HOST, USER, pass, DB);
 

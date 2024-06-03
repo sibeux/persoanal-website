74a8d51f95a2b623d525f3e395aba154e1f4c616
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-06-04 04:57:20 +0700

    Refactor video thumbnail logic in index.php

--- project/transactionApp/index.php
+++ project/transactionApp/index.php
@@ -3,12 +3,12 @@ session_start();
 
 if (!isset($_SESSION["login"])) {
     echo "<script>location.href = 
-    './transactionApp/views/login.php';
+    './views/login.php';
     </script>";
     exit;
 } else{
     echo "<script>location.href = 
-    './transactionApp/views/home.php';
+    './views/home.php';
     </script>";
     exit;
 }

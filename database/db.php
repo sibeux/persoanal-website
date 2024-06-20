1f6fbe511b762f2e0570120955258b6cdfe5b582
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-06-20 18:05:54 +0700

    Refactor database connection credentials

--- database/db.php
+++ database/db.php
@@ -1,9 +1,14 @@
 <?php
 
+// define('HOST', 'localhost');
+// define('SIBEUX', 'root');
+// define('pass', '');
+// define('DB', 'web_porto');
+
 define('HOST', 'localhost');
-define('SIBEUX', 'root');
-define('pass', '');
-define('DB', 'web_porto');
+define('SIBEUX', 'sibk1922_cbux');
+define('pass', '1NvgEHFnwvDN96');
+define('DB', 'sibk1922_cloud_music');
 
 $db = new mysqli(HOST, SIBEUX, pass, DB);
 

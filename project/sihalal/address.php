3a9fd7e12154b5913d82fb11e9d95f0a4c91cb6a
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-10-05 06:44:01 +0700

    Signed-off-by: Nasrul Wahabi <wahabinasrul@gmail.com>

---
+++ project/sihalal/address.php
@@ -0,0 +1,81 @@
+<?php
+
+include './database/db.php';
+
+function sendUserAddress($db, $address)
+{
+    // Mendapatkan data JSON dari body request
+    $input = file_get_contents('php://input');
+
+    // Mengubah JSON menjadi array PHP
+    $data = json_decode($input, true);
+
+    // Cek apakah data berhasil di-decode
+    if ($data === null) {
+        // Jika gagal decode JSON, tangani error
+        http_response_code(400); // Bad Request
+        echo json_encode(['message' => 'Invalid JSON data']);
+        exit;
+    }
+
+    if (
+        $stmt = $db->prepare('INSERT INTO `alamat` (id_alamat, id_user, nama_penerima, 
+    nomor_penerima, label_alamat, provinsi, id_provinsi, kota, id_kota, kode_pos, 
+    detail_alamat, jalan_alamat, pinpoint_alamat, is_utama, is_toko) 
+        VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);')
+    ) {
+        // Mengakses data yang dikirim (misalnya, 'name', 'email', dan 'age')
+        $id_user = $data['id_user'] ?? '';
+        $receipt_name = $data['receipt_name'] ?? '';
+        $receipt_phone = $data['receipt_phone'] ?? '';
+        $label_address = $data['label_address'] ?? '';
+        $province = $data['province'] ?? '';
+        $id_province = $data['id_province'] ?? '';
+        $city = $data['city'] ?? '';
+        $id_city = $data['id_city'] ?? '';
+        $postal_code = $data['postal_code'] ?? '';
+        $detail_address = $data['detail_address'] ?? '';
+        $street_address = $data['street_address'] ?? '';
+        $pin_point = $data['pin_point'] ?? '';
+        $is_primary_address = $data['is_primary_address'] ?? '';
+        $is_store_address = $data['is_store_address'] ?? '';
+
+        // hati-hati sama koma di bind_param terakhir, njir.
+        $stmt->bind_param(
+            'ssssssssssssss',
+            $id_user,
+            $receipt_name,
+            $receipt_phone,
+            $label_address,
+            $province,
+            $id_province,
+            $city,
+            $id_city,
+            $postal_code,
+            $detail_address,
+            $street_address,
+            $pin_point,
+            $is_primary_address,
+            $is_store_address
+        );
+        $stmt->execute();
+
+        // registration successful
+        $response = ["status" => "success"];
+        echo json_encode($response);
+    } else {
+        $response = ["status" => "failed"];
+        echo json_encode($response);
+        echo 'Could not prepare statement!';
+    }
+}
+
+switch ($_POST['method']) {
+    case 'send_user_address':
+        sendUserAddress($db, $_POST['address']);
+        break;
+    default:
+        break;
+}
+
+$db->close();
--- project/sihalal/user.php
+++ project/sihalal/user.php
@@ -11,60 +11,9 @@ function getUserData($email)
     $sql = "SELECT * FROM user WHERE email_user = '" . $email . "'";
 }
 
-function sendUserAddress($email, $address)
-{
-    global $sql;
-
-    // Mendapatkan data JSON dari body request
-    $input = file_get_contents('php://input');
-
-    // Mengubah JSON menjadi array PHP
-    $data = json_decode($input, true);
-
-    // Cek apakah data berhasil di-decode
-    if ($data === null) {
-        // Jika gagal decode JSON, tangani error
-        http_response_code(400); // Bad Request
-        echo json_encode(['message' => 'Invalid JSON data']);
-        exit;
-    }
-
-    // Mengakses data yang dikirim (misalnya, 'name', 'email', dan 'age')
-    $id_user = $data['id_user'] ?? '';
-    $receipt_name = $data['receipt_name'] ?? '';
-    $receipt_phone = $data['receipt_phone'] ?? '';
-    $label_address = $data['label_address'] ?? '';
-    $province = $data['province'] ?? '';
-    $id_province = $data['id_province'] ?? '';
-    $city = $data['city'] ?? '';
-    $id_city = $data['id_city'] ?? '';
-    $postal_code = $data['postal_code'] ?? '';
-    $detail_address = $data['detail_address'] ?? '';
-    $street_address = $data['street_address'] ?? '';
-    $pin_point = $data['pin_point'] ?? '';
-    $is_primary_address = $data['is_primary_address'] ?? '';
-    $is_store_address = $data['is_store_address'] ?? '';
-
-    $sql = "INSERT INTO alamat (id_alamat, id_user, nama_penerima, 
-    nomor_penerima, label_alamat, provinsi, id_provinsi, kota, id_kota, kode_pos, 
-    detail_alamat, jalan_alamat, pinpoint_alamat, is_utama, is_toko) 
-    VALUES (NULL, '$id_user', '$receipt_name', '$receipt_phone', '$label_address', '$province', 
-    '$id_province', '$city', '$id_city', '$postal_code', '$detail_address', 
-    '$street_address', '$pin_point', '$is_primary_address', '$is_store_address')";
-
-}
-
 switch ($_GET['method']) {
     case 'get_user_data':
         getUserData($_GET['email']);
-        break;
-    default:
-        break;
-}
-
-switch ($_POST['method']) {
-    case 'send_user_address':
-        sendUserAddress($_POST['email'], $_POST['address']);
         break;
     default:
         break;

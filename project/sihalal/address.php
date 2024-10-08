afe32d21b28a2021da618e146062edef37f9a021
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-10-08 13:28:29 +0700

    Signed-off-by: Nasrul Wahabi <wahabinasrul@gmail.com>

--- project/sihalal/address.php
+++ project/sihalal/address.php
@@ -7,6 +7,7 @@ $data = [];
 $id_user = '';
 $sql = '';
 
+$id_address = '';
 $id_primary = '';
 $id_store = '';
 $reset_primary = 'false';
@@ -28,6 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
     $method = $data['method'] ?? '';
+    $id_address = $data['id_address'] ?? '';
     $id_primary = $data['id_primary'] ?? '';
     $id_store = $data['id_store'] ?? '';
     $reset_primary = $data['reset_primary'] ?? 'false';
@@ -47,14 +49,14 @@ function sendUserAddress($db, $data)
     global $reset_primary;
     global $reset_store;
 
-    if ($id_primary != '' && $reset_primary == 'true'){
+    if ($id_primary != '' && $reset_primary == 'true') {
         if ($stmt = $db->prepare('UPDATE alamat SET is_utama = "false" WHERE id_alamat = ?;')) {
             $stmt->bind_param('s', $id_primary);
             $stmt->execute();
         }
     }
 
-    if ($id_store != '' && $reset_store == 'true'){
+    if ($id_store != '' && $reset_store == 'true') {
         if ($stmt = $db->prepare('UPDATE alamat SET is_toko = "false" WHERE id_alamat = ?;')) {
             $stmt->bind_param('s', $id_store);
             $stmt->execute();
@@ -101,7 +103,7 @@ function sendUserAddress($db, $data)
             $is_primary_address,
             $is_store_address
         );
-        
+
         $stmt->execute();
 
         // registration successful
@@ -113,7 +115,96 @@ function sendUserAddress($db, $data)
     }
 }
 
-function getUserAddress($id_user){
+function updateUserAddress($db, $data)
+{
+    $address = $data['address'] ?? [];
+    global $id_address;
+    global $id_primary;
+    global $id_store;
+    global $reset_primary;
+    global $reset_store;
+
+    if ($id_primary != '' && $reset_primary == 'true') {
+        if ($stmt = $db->prepare('UPDATE alamat SET is_utama = "false" WHERE id_alamat = ?;')) {
+            $stmt->bind_param('s', $id_primary);
+            $stmt->execute();
+        }
+    }
+
+    if ($id_store != '' && $reset_store == 'true') {
+        if ($stmt = $db->prepare('UPDATE alamat SET is_toko = "false" WHERE id_alamat = ?;')) {
+            $stmt->bind_param('s', $id_store);
+            $stmt->execute();
+        }
+    }
+
+    if (
+        $stmt = $db->prepare('UPDATE alamat 
+    SET nama_penerima = ?, 
+        nomor_penerima = ?, 
+        label_alamat = ?, 
+        provinsi = ?, 
+        id_provinsi = ?, 
+        kota = ?, 
+        id_kota = ?, 
+        kode_pos = ?, 
+        detail_alamat = ?, 
+        jalan_alamat = ?, 
+        pinpoint_alamat = ?, 
+        is_utama = ?, 
+        is_toko = ?
+    WHERE id_alamat = ?;
+')
+    ) {
+        // Mengakses data yang dikirim
+        $receipt_name = $address['receipt_name'] ?? '';
+        $receipt_phone = $address['receipt_phone'] ?? '';
+        $label_address = $address['label_address'] ?? '';
+        $province = $address['province'] ?? '';
+        $id_province = $address['id_province'] ?? '';
+        $city = $address['city'] ?? '';
+        $id_city = $address['id_city'] ?? '';
+        $postal_code = $address['postal_code'] ?? '';
+        $detail_address = $address['detail_address'] ?? '';
+        $street_address = $address['street_address'] ?? '';
+        $pin_point = $address['pin_point'] ?? '';
+        $is_primary_address = $address['is_primary_address'] ?? '';
+        $is_store_address = $address['is_store_address'] ?? '';
+
+        $id_address = $address['id_address'] ?? '';
+
+        // hati-hati sama koma di bind_param terakhir, njir.
+        $stmt->bind_param(
+            'ssssssssssssss',
+            $receipt_name,
+            $receipt_phone,
+            $label_address,
+            $province,
+            $id_province,
+            $city,
+            $id_city,
+            $postal_code,
+            $detail_address,
+            $street_address,
+            $pin_point,
+            $is_primary_address,
+            $is_store_address,
+            $id_address
+        );
+
+        $stmt->execute();
+
+        // registration successful
+        $response = ["status" => "success"];
+        echo json_encode($response);
+    } else {
+        $response = ["status" => "failed"];
+        echo json_encode($response);
+    }
+}
+
+function getUserAddress($id_user)
+{
     global $sql;
 
     $sql = "SELECT * FROM alamat WHERE id_user = $id_user";
@@ -122,6 +213,9 @@ function getUserAddress($id_user){
 switch ($method) {
     case 'send_user_address':
         sendUserAddress($db, $data);
+        break;
+    case 'update_user_address':
+        updateUserAddress($db, $data);
         break;
     case 'get_user_address':
         getUserAddress($id_user);
